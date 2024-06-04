a895ddea707e9eb911f0b7f155763a99d0fbbb63
Erick20247 <Torot0086@gmail.vom>
2024-06-03 20:31:18 -0400

    eh creado el Delete

---
+++ Migrations/20240604001543_Inicial2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ap1_P1_erick.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
